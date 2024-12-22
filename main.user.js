79085286d94af2c2c6eccf8af3a96d0334528b1b
Lesker <85740118+lesker000@users.noreply.github.com>
2024-12-22 05:44:01 +0100

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         DC eve40 pinger
+// @name         DCMonsterAlert
 // @namespace    http://tampermonkey.net/
-// @version      2024-12-18
-// @description  try to take over the world!
+// @version      2024-12-22
+// @description  Dodatek ma wołać na dc po pojawieniu się potwora z listy
 // @author       Lesker
 // @match        https://tempest.margonem.pl/
 // @grant        none
@@ -11,14 +11,11 @@
 /*
 global API
 global Engine
-
 */
 
-/*
-normal-monster
-elita1
-*/
-
+function lg(text){
+    console.log(text);
+}
 
 class MobModel {
     constructor(name, webhookUrl, roleID) {
@@ -27,44 +24,70 @@ class MobModel {
         this.roleID = roleID;
         this.roleStr = `<@&${roleID}>`;
     }
-    checkNpc(n) {
-        if (n.d.type == 2) {
-            if (n.d.nick != this.name) {
-                return false;
-            }
+    checkNpc(n){
+        if (n.d.nick == this.name) {
             return true;
         }
-        return false;
+        return false
+    }
+}
+
+class Model{
+    constructor(){
+        this.mobs=[
+            new MobModel(
+                "Świąteczny Cerber",
+                "https://discord.com/api/webhooks/1319411938228441209/t9LkmWDBXqOCr1T_uMJwaYDcgGoial_uwgZmDIwiUKbNWJ7f0IfUkPE0uEQauv_EfI2R",
+                "1319069418508128319",
+            ),
+            new MobModel(
+                "Świąteczny Tarrol Agze",
+                "https://discord.com/api/webhooks/1319417375497654343/U5sW_og3js6RZHojKoqBF-NnKznK_zSviFL21wRjFaO25rGy943ZKpWfRBYI_KbhpPOL",
+                "1319042994711826433",
+            ),
+        ]
+        this.mob = null;
+    }
+    checkMobs(npc){
+        if(npc.d.type != 4){
+            for(let i=0; i<this.mobs.length;i++){
+                if(this.mobs[i].checkNpc(npc)){
+                    this.mob = this.mobs[i];
+                    return true;
+                   }
+            }
+        }
+        return false
     }
 }
 
 class Controller {
-    constructor(mobModel) {
-        this.mobModel = mobModel;
+    constructor(model) {
+        this.model = model;
         this.run();
     }
     run(){
-        // Sprawdzanie nowych mobów na mapie
         API.addCallbackToEvent("newNpc", (npc) => {
-            if (this.mobModel.checkNpc(npc)) {
-                console.log(npc.d.nick, npc.getKind(), npc.d.wt, npc);
-                this.sendMessage(this.wordMessage(npc));
+            if(this.model.checkMobs(npc)){
+                let mess = this.wordMessage(npc);
+                lg(mess);
+                this.sendMessage(mess);
             }
         });
     }
 
     wordMessage(n) {
-        console.log(this.mobModel.roleStr);
-        let rolePing = this.mobModel.roleStr;
-        let mobName = `${this.mobModel.name} ${this.mobModel.d.lvl}lv`;
+        let rolePing = this.model.mob.roleStr;
+        let mobName = `${this.model.mob.name} ${n.d.lvl}lv`;
         let location = `${Engine.map.d.name} (${n.d.x},${n.d.y})`;
         let hero = `${Engine.hero.d.nick} ${Engine.hero.d.lvl}lvl`;
-        return `${rolePing} ${mobName} - ${location} - ${hero}`;}
+        return `${rolePing} ${mobName} - ${location} - ${hero}`;
+        }
 
     sendMessage(message) {
         console.log(message);
         const request = new XMLHttpRequest();
-        let webhookURL = this.mobModel.webhookUrl;
+        let webhookURL = this.model.mob.webhookUrl;
         request.open("POST", webhookURL, true);
         request.setRequestHeader("Content-Type", "application/json");
         const payload = {
@@ -85,21 +108,11 @@ class Controller {
 
         request.send(JSON.stringify(payload));
     }
+
 }
 
 (function() {
     'use strict';
-    /*
-    let mobModel = new MobModel(
-        "Tyrtajos",
-        "https://discord.com/api/webhooks/1319411938228441209/t9LkmWDBXqOCr1T_uMJwaYDcgGoial_uwgZmDIwiUKbNWJ7f0IfUkPE0uEQauv_EfI2R",
-        "1319069418508128319",
-    );
-    */
-    let mobModel = new MobModel(
-        "Świąteczny Cerber",
-        "https://discord.com/api/webhooks/1319417375497654343/U5sW_og3js6RZHojKoqBF-NnKznK_zSviFL21wRjFaO25rGy943ZKpWfRBYI_KbhpPOL",
-        "1319042994711826433",
-    );
-    let program = new Controller(mobModel);
+    let model = new Model()
+    let program = new Controller(model);
 })();
