89359e4c07a06ba542706e995d08c9e39da4bae6
Lesker <85740118+lesker000@users.noreply.github.com>
2024-12-22 02:46:07 +0100

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,63 +1,105 @@
-class ToggleButton{
-  constructor() {
-    this.isActive = false;
-    this.createButton();
-  }
+// ==UserScript==
+// @name         DC eve40 pinger
+// @namespace    http://tampermonkey.net/
+// @version      2024-12-18
+// @description  try to take over the world!
+// @author       Lesker
+// @match        https://tempest.margonem.pl/
+// @grant        none
+// ==/UserScript==
 
-  createButton() {
-    const button = document.createElement('button');
-    button.id = 'toggleButton';
-    button.innerHTML = 'Start';
+/*
+global API
+global Engine
 
-    // Ustawienia przycisku
-    button.style.position = 'fixed';
-    button.style.top = localStorage.getItem('buttonTop') || '10px';
-    button.style.left = localStorage.getItem('buttonLeft') || '10px';
-    button.style.zIndex = '9999';
-    button.style.padding = '10px';
-    button.style.backgroundColor = 'red';
-    button.style.color = 'white';
-    button.style.border = 'none';
-    button.style.cursor = 'pointer';
+*/
 
-    button.addEventListener('click', () => {
-      this.isActive = !this.isActive;
-      button.innerHTML = this.isActive ? 'Stop' : 'Start';
-      button.style.backgroundColor = this.isActive ? 'green' : 'red';
-    });
+/*
+normal-monster
+elita1
+*/
 
-    // Funkcjonalność przeciągania
-    button.addEventListener('mousedown', function(e) {
-      let shiftX = e.clientX - button.getBoundingClientRect().left;
-      let shiftY = e.clientY - button.getBoundingClientRect().top;
 
-      function moveAt(pageX, pageY) {
-        button.style.left = pageX - shiftX + 'px';
-        button.style.top = pageY - shiftY + 'px';
-      }
-
-      function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-      }
-
-      document.addEventListener('mousemove', onMouseMove);
-
-      button.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        button.onmouseup = null;
-        localStorage.setItem('buttonTop', button.style.top);
-        localStorage.setItem('buttonLeft', button.style.left);
-      };
-    });
-
-    button.ondragstart = function() {
-      return false;
-    };
-
-    document.body.appendChild(button);
-  }
-
-  isButtonActive() {
-    return this.isActive;
-  }
+class MobModel {
+    constructor(name, webhookUrl, roleID) {
+        this.name = name;
+        this.webhookUrl = webhookUrl;
+        this.roleID = roleID;
+        this.roleStr = `<@&${roleID}>`;
+    }
+    checkNpc(n) {
+        if (n.d.type == 2) {
+            if (n.d.nick != this.name) {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 }
+
+class Controller {
+    constructor(mobModel) {
+        this.mobModel = mobModel;
+        this.run();
+    }
+    run(){
+        // Sprawdzanie nowych mobów na mapie
+        API.addCallbackToEvent("newNpc", (npc) => {
+            if (this.mobModel.checkNpc(npc)) {
+                console.log(npc.d.nick, npc.getKind(), npc.d.wt, npc);
+                this.sendMessage(this.wordMessage(npc));
+            }
+        });
+    }
+
+    wordMessage(n) {
+        console.log(this.mobModel.roleStr);
+        let rolePing = this.mobModel.roleStr;
+        let mobName = `${this.mobModel.name} ${this.mobModel.d.lvl}lv`;
+        let location = `${Engine.map.d.name} (${n.d.x},${n.d.y})`;
+        let hero = `${Engine.hero.d.nick} ${Engine.hero.d.lvl}lvl`;
+        return `${rolePing} ${mobName} - ${location} - ${hero}`;}
+
+    sendMessage(message) {
+        console.log(message);
+        const request = new XMLHttpRequest();
+        let webhookURL = this.mobModel.webhookUrl;
+        request.open("POST", webhookURL, true);
+        request.setRequestHeader("Content-Type", "application/json");
+        const payload = {
+            content: `${message}`
+        };
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 300) {
+                console.log("Wiadomość wysłana: ", request.responseText);
+            } else {
+                console.error("Błąd przy wysyłaniu wiadomości: ", request.statusText);
+            }
+        };
+
+        request.onerror = function() {
+            console.error("Błąd sieci");
+        };
+
+        request.send(JSON.stringify(payload));
+    }
+}
+
+(function() {
+    'use strict';
+    /*
+    let mobModel = new MobModel(
+        "Tyrtajos",
+        "https://discord.com/api/webhooks/1319411938228441209/t9LkmWDBXqOCr1T_uMJwaYDcgGoial_uwgZmDIwiUKbNWJ7f0IfUkPE0uEQauv_EfI2R",
+        "1319069418508128319",
+    );
+    */
+    let mobModel = new MobModel(
+        "Świąteczny Cerber",
+        "https://discord.com/api/webhooks/1319417375497654343/U5sW_og3js6RZHojKoqBF-NnKznK_zSviFL21wRjFaO25rGy943ZKpWfRBYI_KbhpPOL",
+        "1319042994711826433",
+    );
+    let program = new Controller(mobModel);
+})();
